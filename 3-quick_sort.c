deb44a34ec285cdb1f6b21d559743400749f1781
Ofallbrains <ofallbrains@gmail.com>
2023-12-19 11:05:19 -0800

    sorting

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,72 +1,73 @@
 #include "sort.h"
+
 /**
- * quick_sort - sorts an array of integers in ascending
- * @array: pointer
- * @size: array
- * Return:
+ * swap_items - Swaps two items in an array.
+ * @array: The array to modify.
+ * @l: The index of the left item.
+ * @r: The index of the right item.
+ */
+void swap_items(int *array, size_t l, size_t r)
+{
+	int tmp;
+
+	if (array != NULL)
+	{
+		tmp = array[l];
+		array[l] = array[r];
+		array[r] = tmp;
+	}
+}
+
+/**
+ * quick_sort_range_lomuto - Sorts a sub array using the
+ * quick sort algorithm and Lomuto's partition scheme.
+ * @array: The array containing the sub-array.
+ * @low: The starting position of the sub-array.
+ * @high: The ending position of the sub-array.
+ * @size: The length of the array.
+ */
+void quick_sort_range_lomuto(int *array, size_t low, size_t high, size_t size)
+{
+	size_t k, i;
+	int pivot;
+
+	if ((low >= high) || (array == NULL))
+		return;
+	pivot = array[high];
+	k = low;
+	for (i = low; i < high; i++)
+	{
+		if (array[i] <= pivot)
+		{
+			if (k != i)
+			{
+				swap_items(array, k, i);
+				print_array(array, size);
+			}
+			k++;
+		}
+	}
+	if (k != high)
+	{
+		swap_items(array, k, high);
+		print_array(array, size);
+	}
+	if (k - low > 1)
+		quick_sort_range_lomuto(array, low, k - 1, size);
+	if (high - k > 1)
+		quick_sort_range_lomuto(array, k + 1, high, size);
+}
+
+/**
+ * quick_sort - Sorts an array using the quick sort algorithm
+ * and Lomuto's partition scheme.
+ * @array: The array to sort.
+ * @size: The length of the array.
  */
 void quick_sort(int *array, size_t size)
 {
-if (array == NULL)
-if (size < 2)
-return;
-lomuto_alg(array, 0, size - 1, size);
-}
-/**
- * lomuto_alg - recursive
- * @point_array: pointer
- * @launch: starting
- * @close: ending
- * @size: array
- *
- * Return:
- */
-void lomuto_alg(int *point_array, int launch, int close, size_t size)
-{
-	int piv_;
-
-	if (launch < close)
+	if (array != NULL)
 	{
-		piv_ = partition(point_array, launch, close, size);
-		lomuto_alg(point_array, launch, piv_ - 1, size);
-		lomuto_alg(point_array, piv_ + 1, close, size);
+		quick_sort_range_lomuto(array, 0, size - 1, size);
 	}
-}
-/**
- * partition - partitions array
- *
- * @point_array: pointer
- * @launch: starting
- * @close: ending
- * @size: array
- * Return: aux
- */
-int partition(int *point_array, size_t launch, size_t close, size_t size)
-{
-	size_t i = launch - 1;
-	size_t k;
-	int tmp_1 = 0;
-	int aux_1 = 0;
-	size_t aux_2 = 0;
-
-	for (k = launch; k < close; k++)
-	{
-		if (point_array[k] < point_array[close])
-		{
-			i++;
-			tmp_1 = point_array[i];
-			point_array[i] = point_array[k];
-			point_array[k] = tmp_1;
-			if (point_array[k] != point_array[i])
-				print_array(point_array, size);
-		}
-	}
-	i++;
-	aux_2 = i;
-	aux_1 = point_array[i];
-	point_array[i] = point_array[close];
-	point_array[close] = aux_1;
-	if (point_array[k] != point_array[i])
-		print_array(point_array, size);
-	return (aux_2);
 }
